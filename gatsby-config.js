61c8eca3d8c34366e63ee80b94c74be1934f6955
IssaDia <issadiapro@gmail.com>
2021-06-01 22:14:58 +0200

    changed contentful env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,8 +8,8 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        accessToken: process.env.GATSBY_ACCESS_TOKEN,
-        spaceId: process.env.GATSBY_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
       },
     },
     "gatsby-plugin-gatsby-cloud",
